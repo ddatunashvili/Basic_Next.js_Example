1ba0bac1efc13efc81977063c3218bb4a86abf58
ddatunashvili <davitidatunashvili98@gmail.com>
2024-09-04 06:58:33 +0400

    config fix

--- config/index.js
+++ config/index.js
@@ -1,3 +1,9 @@
 const dev = process.env.NODE_ENV !== "production"
+const url = process.env.URL
+
 
 export const server = dev ? 'http://localhost:3000': 'https://gela.com'
+
+if (url){
+    server = url
+}
